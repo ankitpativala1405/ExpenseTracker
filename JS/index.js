7d9df4f8b9b72db44d87918c5fbfdc35c9357e77
Your Name <you@example.com>
2025-05-08 14:43:30 +0530

    update

--- JS/index.js
+++ JS/index.js
@@ -8,6 +8,8 @@ window.onload = () => {
   document.getElementById("ExpenceBalance").innerHTML = `₹ ${ExpenceBalance}`;
 
   document.getElementById("ShowBalance").innerHTML = `₹ ${ShowBalance}`;
+
+  document.getElementById("date").valueAsDate = new Date();
 };
 
 document.getElementById("IncomeDiv").addEventListener("click", () => {
@@ -19,3 +21,49 @@ document.getElementById("IncomeDiv").addEventListener("click", () => {
     return;
   }
 });
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+document.getElementById("addtransactionform").addEventListener("submit", () => {
+  let description = document.getElementById("description").value;
+  let category = document.getElementById("category").value;
+  let date = document.getElementById("date").value;
+  let amount = document.getElementById("amount").value;
+
+  let transaction = {
+    id: Date.now(),
+    description: description,
+    category: category,
+    date: date,
+    amount: amount,
+    timestamp: new Date().getDate(),
+  };
+
+  transactions.push(transaction);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+});
+
+const Uimaker = () => {
+  document.getElementById("transactionList").innerHTML = "";
+
+  transactions.map((ele) => {
+    console.log("ele", ele);
+
+    let li = document.createElement("li");
+
+    let descSpan = document.createElement("span");
+    descSpan.innerHTML = `${ele.category} <small class="text-muted">${ele.description}</small>`;
+
+    let amountSpan = document.createElement("span");
+    amountSpan.classList.add("expense");
+    amountSpan.textContent = `- ₹${ele.amount}`;
+
+    li.append(descSpan, amountSpan);
+
+    ExpenceBalance = ExpenceBalance + ele.amount;
+    document.getElementById("transactionList").appendChild(li);
+  });
+
+};
+
+Uimaker();
